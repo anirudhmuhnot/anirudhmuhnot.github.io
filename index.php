f75357ef1f6991a3415eb0aca5e348f79698d065
anirudhmuhnot <muhnot@icloud.com>
2018-03-30 07:31:26 +0530

    nav fixed

--- index.php
+++ index.php
@@ -11,25 +11,8 @@
   <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
 <body>
-  <div class="navbar-fixed">
-<nav class="grey darken-4" role="navigation">
-    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Anirudh Muhnot</a>
-      <ul class="right hide-on-med-and-down">
-        <li><a href="index.php">Home</a></li>
-        <li><a href="blog.php">Blog</a></li>
-        <li><a href="projects.php">All Projects</a></li>
-      </ul>
-
-      <ul id="nav-mobile" class="side-nav">
-        <li><a href="index.php">Home</a></li>
-        <li><a href="blog.php">Blog</a></li>
-        <li><a href="projects.php">All Projects</a></li>
-      </ul>
-      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
-    </div>
-  </nav>
-</div>
-  <div class="section no-pad-bot" id="index-banner">
+<?php include "nav.php"; ?>
+<div class="section no-pad-bot" id="index-banner">
     <div class="container">
       <br><br>
       <h1 class="header center orange-text">Wecome</h1>
