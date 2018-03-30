c5910f2f30c8815d7b56b697d6e0ea52d50d3cb5
anirudhmuhnot <muhnot@icloud.com>
2018-03-30 12:53:57 +0530

    dashboard final

--- myWebsite/projects/1attendancedashboard.php
+++ myWebsite/projects/1attendancedashboard.php
@@ -15,51 +15,51 @@
 <?php include "../nav.php"; ?>
 <div class="row">
   <div class="col s12">
-    <h1>
+    <h3>
     Attendance Dashboard
-    </h1>
+    </h3>
   </div>
 </div>
 
 
 <div class="row">
-  <h4>Top 7 stundets
+  <h4>Top 7 students
   <div class="col s12">
-      <iframe src="http://35.231.52.18:9601/#/notebook/2DACT4YCF/paragraph/20180315-121736_630062287?asIframe"></iframe>    
+      <iframe src="http://35.231.52.18:9601/#/notebook/2DACT4YCF/paragraph/20180330-064139_1785983861?asIframe" width="99%" height='400px' scrolling="no" frameborder='0'></iframe>    
   </div>
 </div>
 
 <div class="row">
   <h4>Date-wise attendance of two different students</h4>
   <div class="col s12">
-    <iframe src="http://35.231.52.18:9601/#/notebook/2DACT4YCF/paragraph/20180318-123128_1627350119?asIframe"></iframe>
+    <iframe src="http://35.231.52.18:9601/#/notebook/2DACT4YCF/paragraph/20180318-123128_1627350119?asIframe" height='450px' width="99%" frameborder='0' scrolling="no"></iframe>
   </div>
 </div>
 
 <div class="row">
   <h4>Bar Plot of student's attendance</h4>
   <div class="col s12">
-    <iframe src="http://35.231.52.18:9601/#/notebook/2DACT4YCF/paragraph/20180318-123126_1735154516?asIframe"></iframe>
+    <iframe src="http://35.231.52.18:9601/#/notebook/2DACT4YCF/paragraph/20180318-123126_1735154516?asIframe" height='400px' frameborder='0' width="99%" scrolling="no"></iframe>
   </div>
 </div>
 
 <div class="row">
   <h4>Line chart of student's present percentage</h4>
   <div class="col s12">
-    <iframe src="http://35.231.52.18:9601/#/notebook/2DACT4YCF/paragraph/20180316-194604_1749034273?asIframe"></iframe>
+    <iframe src="http://35.231.52.18:9601/#/notebook/2DACT4YCF/paragraph/20180316-194604_1749034273?asIframe"  height='400px' frameborder='0' width="99%" scrolling="no"></iframe>
   </div>
 
 <div class="row">
   <h4>Student's attendance compared to class average</h4>
   <div class="col s12">
-    <iframe src="http://35.231.52.18:9601/#/notebook/2DACT4YCF/paragraph/20180315-121708_852935793?asIframe"></iframe>
+    <iframe src="http://35.231.52.18:9601/#/notebook/2DACT4YCF/paragraph/20180315-121708_852935793?asIframe"  height='400px' frameborder='0' width="99%" scrolling="no"></iframe>
   </div>
 </div>
 
 <div class="row">
   <h4>Pie chart of student's present percentage</h4>
   <div class="col s12">
-    <iframe src="http://35.231.52.18:9601/#/notebook/2DACT4YCF/paragraph/20180316-171525_1104347662?asIframe"></iframe>
+    <iframe src="http://35.231.52.18:9601/#/notebook/2DACT4YCF/paragraph/20180316-171525_1104347662?asIframe" height='700px' frameborder='0' width="99%" scrolling="no"></iframe>
   </div>
 </div>
 
