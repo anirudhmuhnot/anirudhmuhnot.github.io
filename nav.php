d1bab7b68ce4abe4197be06a32d71532f6e4f6ce
Anirudh Muhnot <anirudhmuhnot@Anirudhs-MacBook-Air.local>
2018-05-30 01:03:07 +0530

    update

--- nav.php
+++ nav.php
@@ -1,6 +1,6 @@
 <div class="navbar-fixed">
 <nav class="grey darken-4" role="navigation">
-    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Anirudh Muhnot</a>
+    <div class="nav-wrapper container"><a id="logo-container" href="index.php" class="brand-logo">Anirudh Muhnot</a>
       <ul class="right hide-on-med-and-down">
         <li><a href="index.php">Home</a></li>
         <li><a href="blog.php">Blog</a></li>
